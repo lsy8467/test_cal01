82037f544260910b95f83665a43b535ae3c2bf37
Robbie <robbie331@>
2024-07-31 05:18:30 +0900

    반복문을 사용해 exit 입력 전까지 계속해서 계산

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,28 +7,35 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
 
-        if (operator == '+') {
-            System.out.println(num1 + num2);
-        } else if (operator == '-') {
-            System.out.println(num1 - num2);
-        } else if (operator == '*') {
-            System.out.println(num1 * num2);
-        } else if (operator == '/') {
-            if (num2 != 0) {
-                System.out.println(num1 / num2);
+            if (operator == '+') {
+                System.out.println(num1 + num2);
+            } else if (operator == '-') {
+                System.out.println(num1 - num2);
+            } else if (operator == '*') {
+                System.out.println(num1 * num2);
+            } else if (operator == '/') {
+                if (num2 != 0) {
+                    System.out.println(num1 / num2);
+                } else {
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                }
             } else {
-                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                System.out.println("잘못된 값을 입력했습니다.");
             }
-        } else {
-            System.out.println("잘못된 값을 입력했습니다.");
+            System.out.println("더 계산하시겠습니까? (exit 입력시 종료)");
+            String input = sc.next();
+            if (input.equals("exit")) {
+                break;
+            }
         }
     }
 }
